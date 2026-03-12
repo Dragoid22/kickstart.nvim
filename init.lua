68cee005e0a5ab09651bfa7651fb15b7ca67887d
Dragoid22 <andrewjohns2004@gmail.com>
2026-03-12 12:15:28 -0500

    Add "Catppuccine" Theme

--- init.lua
+++ init.lua
@@ -803,20 +803,67 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('catppuccin').setup {
+        flavour = "auto", -- auto, latte, frappe, macchiato, mocha
+        lsp_styles = {
+          underlines = {
+            errors = { 'undercurl' },
+            hints = { 'undercurl' },
+            warnings = { 'undercurl' },
+            information = { 'undercurl' },
+          },
+        },
+        integrations = {
+          aerial = true,
+          alpha = true,
+          cmp = true,
+          dashboard = true,
+          flash = true,
+          fzf = true,
+          grug_far = true,
+          gitsigns = true,
+          headlines = true,
+          illuminate = true,
+          indent_blankline = { enabled = true },
+          leap = true,
+          lsp_trouble = true,
+          mason = true,
+          mini = true,
+          navic = { enabled = true, custom_bg = 'lualine' },
+          neotest = true,
+          neotree = true,
+          noice = true,
+          notify = true,
+          snacks = true,
+          telescope = true,
+          treesitter_context = true,
+          which_key = true,
+        },
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = {  }, -- Disable italics in comments
+        },
+        specs = {
+          {
+            "akinsho/bufferline.nvim",
+            optional = true,
+            opts = function(_, opts)
+              if (vim.g.colors_name or ""):find("catppuccin") then
+                opts.highlights = require("catppuccin.special.bufferline").get_theme()
+              end
+            end,
+          },
         },
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
